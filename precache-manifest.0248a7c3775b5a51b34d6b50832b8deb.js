8017e7d66ca105ab3d690cd926d2c717d6cd7f3d
sandhukaran28 <60167401+sandhukaran28@users.noreply.github.com>
2020-12-10 10:44:19 +0530

    Updates

---
+++ precache-manifest.0248a7c3775b5a51b34d6b50832b8deb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02547b79830dd8b218437f661e60d1a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "b6e05510adaa97849e9f",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e05510adaa97849e9f",
+    "url": "/home/static/js/main.30bc0473.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "cc651279b3484ef2676fe10032925c74",
+    "url": "/home/static/media/dp1.cc651279.jpg"
+  },
+  {
+    "revision": "3041d42df7b360124c88f1ac4d1d06f8",
+    "url": "/home/static/media/resume.3041d42d.pdf"
+  }
+]);
